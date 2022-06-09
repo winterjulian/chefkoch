32b33603dd596796617474694662d40b1e7cf4c2
Julian Winter <julian.winter91@gmail.com>
2022-06-09 15:03:37 +0200

    Added correct font;

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
   <component :is="currentView"/>
 </template>
 
